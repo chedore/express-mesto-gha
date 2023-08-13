24f8b12cad16fda605d63c18ccca0253b645b2e7
Svetlana Prozhirova <svetakesha@yandex.ru>
2023-08-13 22:36:37 +0300

    тест 1

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,5 +1,7 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 const { celebrate, Joi } = require('celebrate');
 
+// eslint-disable-next-line no-useless-escape
 const regex = /^(http|https):\/\/(?:www\.)?[a-zA-Z0-9._~\-:?#[\]@!$&'()*+,\/;=]{2,256}\.[a-zA-Z0-9.\/?#-]{2,}$/;
 
 const validateUserProfile = celebrate({
