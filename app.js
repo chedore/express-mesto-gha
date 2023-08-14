759e4485bfd4e55acd55c18d338e2b979b7eb32f
Svetlana Prozhirova <svetakesha@yandex.ru>
2023-08-14 17:16:09 +0300

    тест 21

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const process = require('process');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const rateLimit = require('express-rate-limit');
 
 const { PORT = 3000 } = process.env;
 const express = require('express');
@@ -13,6 +15,16 @@ const errorHandler = require('./middlewares/error-handler');
 const app = express();
 app.use(express.json());
 app.use(helmet());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  // store: ... , // Use an external store for more precise rate limiting
+});
+
+app.use(limiter);
 
 mongoose.connect('mongodb://127.0.0.1:27017/mydb', {});
 
