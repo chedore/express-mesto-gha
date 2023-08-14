8c0ddbf8eb9c69d9fd41e44f58203e569ae40cc0
Svetlana Prozhirova <svetakesha@yandex.ru>
2023-08-14 16:15:06 +0300

    тест 18

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,26 +6,26 @@ const {
 
 // eslint-disable-next-line consistent-return
 const auth = (req, res, next) => {
-  const { authorization } = req.headers;
+  // const { authorization } = req.headers;
 
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res
-      .status(UNAUTHORIZED)
-      .send({ message: 'Необходима авторизация' });
-  }
+  // if (!authorization || !authorization.startsWith('Bearer ')) {
+  //   return res
+  //     .status(UNAUTHORIZED)
+  //     .send({ message: 'Необходима авторизация' });
+  // }
 
-  const token = authorization.replace('Bearer ', '');
-  let payload;
+  // const token = authorization.replace('Bearer ', '');
+  // let payload;
 
-  try {
-    // eslint-disable-next-line no-unused-vars
-    payload = jwt.verify(token, 'some-secret-key');
-  } catch (err) {
-    return res
-      .status(UNAUTHORIZED)
-      .send({ message: 'Необходима авторизация' });
-  }
-  req.user = payload;
+  // try {
+  //   // eslint-disable-next-line no-unused-vars
+  //   payload = jwt.verify(token, 'some-secret-key');
+  // } catch (err) {
+  //   return res
+  //     .status(UNAUTHORIZED)
+  //     .send({ message: 'Необходима авторизация' });
+  // }
+  // req.user = payload;
 
   next();
 };
